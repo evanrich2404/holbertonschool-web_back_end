fb18faac575ac8b24f7fc540f9c61cb40e172cb7
evanrich2404 <evanrich2404@gmail.com>
2023-05-08 23:43:35 -0500

    trying it without comments

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,6 +1,3 @@
-// modifying the code to use const and let
-
-// const is used when the value of the variable is not going to change
 export function taskFirst() {
   const task = 'I prefer const when I can. but';
   return task;
@@ -10,7 +7,6 @@ export function getLast() {
   return ' is okay';
 }
 
-// let is used when the value of the variable is going to change
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
