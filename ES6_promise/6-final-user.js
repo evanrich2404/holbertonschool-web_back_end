60aa08947f1b3fdf660a154cd48987bedfe7c9c8
evanrich2404 <evanrich2404@gmail.com>
2023-05-12 17:51:26 -0500

    fixed error message and reformed whole func

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,15 +2,21 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
-  const user = await signUpUser(firstName, lastName);
-  let photo;
-  try {
-    photo = await uploadPhoto(filename);
-  } catch (err) {
-    photo = null;
-  }
-  return {
-    user,
-    photo,
-  };
+  const user = await signUpUser(firstName, lastName).then((data) => {
+    console.log(data);
+    return ({
+      status: 'fulfilled',
+      value: data,
+    });
+  });
+
+  const photo = await uploadPhoto(filename).catch((err) => {
+    console.log(err.message);
+    return ({
+      status: 'rejected',
+      value: `Error: ${err.message}`,
+    });
+  });
+
+  return ([user, photo]);
 }
