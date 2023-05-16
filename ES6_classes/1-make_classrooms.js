8e6cf23acacc146df8bcab1005ddcb8de03f92ee
evanrich2404 <evanrich2404@gmail.com>
2023-05-16 17:44:24 -0500

    used new class to make a function

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import Classroom from './0-classroom';
+
+export default function initializeRooms() {
+  const rooms = [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34),
+  ];
+  return rooms;
+}
