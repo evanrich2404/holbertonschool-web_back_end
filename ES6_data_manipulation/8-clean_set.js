ffa00fb8b811c8db052c670b3df7fa18cdfd061e
evanrich2404 <evanrich2404@gmail.com>
2023-05-17 19:32:34 -0500

    big dog

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,22 @@
-export default function cleanSet(set, startString) {
+function cleanSet(set, startString) {
+  // Check if startString is a string and not empty
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+
   const result = [];
-  set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+  for (const item of set) {
+    // Check if item is a string before calling startsWith
+    if (typeof item === 'string' && item.startsWith(startString)) {
+      // Only add the rest of the string if it's not empty
+      const restOfString = item.slice(startString.length);
+      if (restOfString !== '') {
+        result.push(restOfString);
+      }
     }
-  });
+  }
+
   return result.join('-');
 }
+
+export default cleanSet;
