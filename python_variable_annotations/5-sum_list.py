61b6fc77d0ebf71afc7ce79745015b36fa369105
evanrich2404 <evanrich2404@gmail.com>
2023-09-07 18:39:07 -0500

    sum of floats using typing's List

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""sum_list module which takes input_list"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns the sum of floats from input_list"""
+    return sum(input_list)
