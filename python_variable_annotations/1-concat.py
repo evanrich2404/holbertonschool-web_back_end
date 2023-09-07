220ae6d4f3bb84cc2a83228e603278931c4bd494
evanrich2404 <evanrich2404@gmail.com>
2023-09-07 17:11:40 -0500

    concatenation of two strings has never been easier

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings together while using type annotations"""
+    return str1 + str2
