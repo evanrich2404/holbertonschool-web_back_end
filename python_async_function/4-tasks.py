892248c6f261e39fc83f55747cfbcd58f4ce4a92
evanrich2404 <evanrich2404@gmail.com>
2023-09-07 21:46:15 -0500

    sorted

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -16,4 +16,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random as Tasks and return list of delays."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    return delays
+    return sorted(delays)
