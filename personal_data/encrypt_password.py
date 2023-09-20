891748827857d7b1ef6d3df479157112d2fdb582
evanrich2404 <evanrich2404@gmail.com>
2023-09-20 02:16:02 -0500

    checking for validity is also epic

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,3 +6,11 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """returns a salted, hashed password, which is a byte string"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """returns a boolean"""
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
