d6e7323fd755f62a33769a856e56d760fa858780
evanrich2404 <evanrich2404@gmail.com>
2023-09-20 23:36:26 -0500

    something aint right

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -62,3 +62,27 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """overloads Auth and retrieves the User instance for a request"""
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        if base64_header is None:
+            return None
+
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        if decoded_header is None:
+            return None
+
+        user_credentials = self.extract_user_credentials(decoded_header)
+        if user_credentials is None:
+            return None
+
+        user_email = user_credentials[0]
+        user_pwd = user_credentials[1]
+
+        user = self.user_object_from_credentials(user_email, user_pwd)
+        return user
