c98242f5f9e71695b463481e2583260e803721ac
evanrich2404 <evanrich2404@gmail.com>
2023-09-20 22:04:08 -0500

    extract them credentials!

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,3 +30,15 @@ class BasicAuth(Auth):
             return decoded_str
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """returns the user email and password from the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        user_pass = decoded_base64_authorization_header.split(':', 1)
+        return user_pass[0], user_pass[1]
