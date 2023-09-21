c98ceb5ca3c920fd77af58019cb9be7bbad30743
evanrich2404 <evanrich2404@gmail.com>
2023-09-20 23:02:25 -0500

    task 10 making user from creds

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """BasicAuth that inherits from Auth"""
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -42,3 +44,20 @@ class BasicAuth(Auth):
             return None, None
         user_pass = decoded_base64_authorization_header.split(':', 1)
         return user_pass[0], user_pass[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+            if not users:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exception:
+            return None
+        return None
