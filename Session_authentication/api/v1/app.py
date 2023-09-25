9ef9fed56a4f735756a616bab067b8e60c66712f
evanrich2404 <evanrich2404@gmail.com>
2023-09-25 17:25:43 +0000

    pycodestyle

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -37,7 +37,8 @@ def before_request_func():
     if not auth.require_auth(request.path, paths):
         return
 
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if (auth.authorization_header(request) is None and
+        auth.session_cookie(request) is None):
         abort(401)
 
     if auth.current_user(request) is None:
