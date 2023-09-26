aa3188163ddbff962f8d8425b98d72c13c86041b
evanrich2404 <evanrich2404@gmail.com>
2023-09-26 19:45:17 +0000

    pycodestyle

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 class User(Base):
     """User class for database table users"""
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False, unique=True)
     hashed_password = Column(String(250), nullable=False)
