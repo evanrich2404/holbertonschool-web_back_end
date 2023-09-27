92c25a735e20115cbb87ae489c1b2ee0feee8d0e
evanrich2404 <evanrich2404@gmail.com>
2023-09-27 02:44:57 +0000

    okay so pylance isn't happy but this might work

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> 'User':
+    def add_user(self, email: str, hashed_password: str) -> 'user.User':
         """Method that saves a new user to the database"""
 
         new_user = User(email=email, hashed_password=hashed_password)
