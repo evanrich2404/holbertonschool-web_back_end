29b5166f62611425da0962aefca2a9f6831c1733
evanrich2404 <evanrich2404@gmail.com>
2023-09-28 01:43:41 +0000

    test with checker time

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python3
 """Auth Module"""
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
     """Method that takes in a password string arguments and returns bytes.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    from user import User
+
+    def register_user(self, email: str, password: str) -> User:
+        user = self._db.find_user_by(email=email)
+        try:
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hash_password = _hash_password(password)
+            user = self._db.add_user(email, hash_password)
+            return user
