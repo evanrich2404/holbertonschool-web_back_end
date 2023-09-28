ce63693ae0abcb60192f46437d00301f1906fd6c
evanrich2404 <evanrich2404@gmail.com>
2023-09-28 03:16:58 +0000

    destroy sess

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -54,6 +54,15 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        if not user_id:
+            return None
+
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
+
 
 def _generate_uuid() -> str:
     """Generates a string representation of a new UUID."""
