7ef2328415506e3859949693068c2ff27e8834c0
evanrich2404 <evanrich2404@gmail.com>
2023-09-28 02:35:26 +0000

    pycodestyle

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -37,10 +37,10 @@ class Auth:
         except NoResultFound:
             return False
 
-    def create_session(self, email:str) -> str:
+    def create_session(self, email: str) -> str:
         user = self._db.find_user_by(email=email)
         session_id = _generate_uuid()
-        
+
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
