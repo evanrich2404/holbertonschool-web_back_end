12b45b0489c5e8ce5110436ae3d9184edc1c5db2
evanrich2404 <evanrich2404@gmail.com>
2023-09-28 02:20:57 +0000

    valid login

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -27,3 +27,11 @@ class Auth:
             hash_password = _hash_password(password)
             user = self._db.add_user(email, hash_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
