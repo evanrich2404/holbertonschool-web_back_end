0d5b8cf090eb4de3e9ece6ed25e82a6c608fce6d
evanrich2404 <evanrich2404@gmail.com>
2023-09-28 02:48:04 +0000

    RESPOND PLS

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -29,5 +29,21 @@ def user():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        abort(400, "email and password fields are required")
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
