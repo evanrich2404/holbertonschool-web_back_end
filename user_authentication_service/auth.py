484ee3d9d1b7f487e0b8c22a7f1ccc86e4893ce8
evanrich2404 <evanrich2404@gmail.com>
2023-09-28 03:14:48 +0000

    get user from the session id yo

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -44,6 +44,15 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
         return session_id
 
+    def get_user_from_session_id(self, session_id: str) -> User or None:
+        if not session_id:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
 
 def _generate_uuid() -> str:
     """Generates a string representation of a new UUID."""
