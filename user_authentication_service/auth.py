d69c7ab6f06ae19112987206941611c97a17dbf6
evanrich2404 <evanrich2404@gmail.com>
2023-09-28 02:33:22 +0000

    AH

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -39,7 +39,9 @@ class Auth:
 
     def create_session(self, email:str) -> str:
         user = self._db.find_user_by(email=email)
-        session_id = _generate_uuid(user)
+        session_id = _generate_uuid()
+        
+        self._db.update_user(user.id, session_id=session_id)
         return session_id
 
 
