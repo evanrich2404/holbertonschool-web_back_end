1c10f9ade830c58a1c6567004333ad0edc5008fd
evanrich2404 <evanrich2404@gmail.com>
2023-09-28 02:11:04 +0000

    added users route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 """Making basic Flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
+
+
 @app.route('/', methods=['GET'])
 def root():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def user():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or not password:
+        abort(400, "email and password fields are required")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
