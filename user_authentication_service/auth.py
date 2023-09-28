121db9ee7c8dd8d77ea18edf8bf220a967a73292
evanrich2404 <evanrich2404@gmail.com>
2023-09-28 03:15:32 +0000

    I swear my blank lines keep getting eaten

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -54,6 +54,7 @@ class Auth:
         except NoResultFound:
             return None
 
+
 def _generate_uuid() -> str:
     """Generates a string representation of a new UUID."""
     return str(uuid.uuid4())
