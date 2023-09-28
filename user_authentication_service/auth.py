90af09e46daf371bcdd26cc92940e05dd9ee9844
evanrich2404 <evanrich2404@gmail.com>
2023-09-28 01:23:33 +0000

    hashed and salted, ready for consumption

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Auth Module"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Method that takes in a password string arguments and returns bytes.
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
