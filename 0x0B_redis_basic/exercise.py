ebad74f0340ca867c7e8c7795bc1444f3db8ccfc
evanrich2404 <evanrich2404@gmail.com>
2023-10-16 01:50:40 +0000

    forgot privacy lol

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,10 +5,10 @@ from typing import Union
 
 class Cache:
     def __init__(self):
-        self.redis = redis.Redis()
-        self.redis.flushdb()
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         random_key = str(uuid.uuid4())
-        self.redis.set(random_key, data)
+        self._redis.set(random_key, data)
         return random_key
