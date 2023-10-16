75d32ebd372aa99afdd2182d2d72eef36b8156b7
evanrich2404 <evanrich2404@gmail.com>
2023-10-16 06:45:29 +0000

    even more doc

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -14,6 +14,7 @@ def count_calls(method: Callable) -> Callable:
     """Count the number of times a method is called."""
     @functools.wraps(method)
     def wrapper(self, *args, **kwargs):
+        """Wrapper function."""
         self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
     return wrapper
@@ -23,6 +24,7 @@ def call_history(method: Callable) -> Callable:
     """Store the history of inputs and outputs for a particular function."""
     @functools.wraps(method)
     def wrapper(self, *args):
+        """Wrapper function."""
         self._redis.rpush("{}:inputs".format(method.__qualname__), str(args))
         output = str(method(self, *args))
         self._redis.rpush(method.__qualname__ + ":outputs", output)
