44019e17dc4c0d6f7af0edc4ad8702951cfda8d2
evanrich2404 <evanrich2404@gmail.com>
2023-10-16 01:46:29 +0000

    class of Cache

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    def __init__(self):
+        self.redis = redis.Redis()
+        self.redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        random_key = str(uuid.uuid4())
+        self.redis.set(random_key, data)
+        return random_key
