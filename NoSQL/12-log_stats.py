bb02eda9bb4d2950da78a247a492bd157fe182d3
evanrich2404 <evanrich2404@gmail.com>
2023-10-18 21:56:05 -0500

    revert

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
-""" 12. Log stats """
+"""Provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
-
-client = MongoClient('mongodb://172.17.0.2:27017')
-collection = client.logs.nginx
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-def log_stats(mongo_collection):
-    """ provides some stats about Nginx logs stored in MongoDB """
-    print("{} logs".format(mongo_collection.count_documents({})))
+def log_stats():
+    """Provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient()
+    db = client.logs
+    logs = db.nginx
+    print("{} logs".format(logs.count_documents({})))
     print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        print("\tmethod {}: {}".format(method, mongo_collection.count_documents({"method": method})))
-
-    print("{} status check".format(mongo_collection.count_documents({"method": "GET", "path": "/status"})))
-
+        print(f"\tmethod {method}: " +
+              f"{logs.count_documents({'method': method})}")
+    print(f"{logs.count_documents({'method': 'GET', 'path': '/status'})} \
+status check")
 
 if __name__ == "__main__":
-    log_stats(collection)
+    log_stats()
