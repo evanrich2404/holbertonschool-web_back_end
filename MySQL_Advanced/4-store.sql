67a1dcb29ec1f115eff6f50314d24c9307c42726
evanrich2404 <evanrich2404@gmail.com>
2023-10-18 20:12:30 -0500

    e

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,15 +1,12 @@
--- Initialize SQL
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
-
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity INT NOT NULL DEFAULT 10
-);
-
-CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(255) NOT NULL,
-    number INT NOT NULL
-);
-
-INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
+-- Task 4 creating a trigger to decrease the quantity of items after adding new orders
+DELIMITER //
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
+//
+DELIMITER ;
